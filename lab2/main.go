5b42b88c4d1c1edbb8a0b09335e6033eaaa99e4e
Stanislav Buldakov <s.buldakov@galament.com>
2017-12-11 20:50:36 +0400

    Lab 2

---
+++ lab2/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sort"
+	"time"
+)
+
+type Input struct {
+	K    int       `json:"K"`
+	Sums []float32 `json:"Sums"`
+	Muls []int     `json:"Muls"`
+}
+
+type Output struct {
+	SumResult    float32   `json:"SumResult"`
+	MulResult    float32   `json:"MulResult"`
+	SortedInputs []float32 `json:"SortedInputs"`
+}
+
+type Config struct {
+	Url string
+}
+
+func GetInputData(config Config) Input {
+	url := fmt.Sprintf("%s/GetInputData", config.Url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var input Input
+	err = json.Unmarshal(body, &input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return input
+}
+
+func WriteAnswer(config Config, output Output) {
+	url := fmt.Sprintf("%s/WriteAnswer", config.Url)
+	b, err := json.Marshal(output)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := bytes.NewReader(b)
+	_, err = http.Post(url, "application/json", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type fl []float32
+
+func (s fl) Len() int {
+	return len(s)
+}
+func (s fl) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s fl) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func Calculate(input Input) Output {
+	var sortedInputs []float32
+	sortedInputs = append(sortedInputs, input.Sums[:len(input.Sums)]...)
+	var sumResults float32 = 0
+	var mulResults float32 = 1.0
+	for i := 0; i < len(input.Sums); i++ {
+		sumResults += input.Sums[i] * float32(input.K)
+	}
+
+	for i := 0; i < len(input.Muls); i++ {
+		mulResults *= float32(input.Muls[i])
+		sortedInputs = append(sortedInputs, float32(input.Muls[i]))
+	}
+	sort.Sort(fl(sortedInputs))
+	var output = Output{
+		SumResult:    sumResults,
+		MulResult:    mulResults,
+		SortedInputs: sortedInputs,
+	}
+
+	return output
+}
+
+func Ping(config Config) int {
+	url := fmt.Sprintf("%s/Ping", config.Url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp.StatusCode
+}
+
+func main() {
+	var port string
+	_, err := fmt.Scanf("%s", &port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	url := fmt.Sprintf("htpp://127.0.0.1:%s/", port)
+	config := Config(url)
+	var status int
+	for {
+		if status != http.StatusOK {
+			break
+		}
+		status = Ping(config)
+		time.Sleep(1 * time.Second)
+	}
+
+	input := GetInputData(config)
+	output := Calculate(input)
+	WriteAnswer(config, output)
+
+}
---
+++ lab2/main_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetInputData(t *testing.T) {
+	jsonString := "{\"K\":10,\"Sums\":[1.01,2.02],\"Muls\":[1,4]}"
+	inputObject := Input{
+		10,
+		[]float32{1.01, 2.02},
+		[]int{1, 4},
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(jsonString))
+	}))
+	defer ts.Close()
+	config := Config{ts.URL}
+	input := GetInputData(config)
+
+	if inputObject.K != input.K {
+		t.Error("K are not Equal")
+	}
+
+	if len(inputObject.Muls) != len(input.Muls) {
+		t.Error("Muls are not Equal")
+	}
+	for i := 0; i < len(input.Muls); i++ {
+		if inputObject.Muls[i] != input.Muls[i] {
+			t.Error("Muls are not Equal")
+		}
+	}
+
+	if len(inputObject.Sums) != len(input.Sums) {
+		t.Error("Sums are not Equal")
+	}
+	for i := 0; i < len(input.Sums); i++ {
+		if inputObject.Sums[i] != input.Sums[i] {
+			t.Error("Sums are not Equal")
+		}
+	}
+}
+
+func TestWriteAnswer(t *testing.T) {
+	output := Output{
+		30.300001,
+		4,
+		[]float32{1, 1.01, 2.02, 4},
+	}
+	outputJson, err := json.Marshal(output)
+	if err != nil {
+		t.Error(err)
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if string(resp) != string(outputJson) {
+			t.Error("JSON strings are not equal")
+		}
+	}))
+	defer ts.Close()
+	config := Config{ts.URL}
+	WriteAnswer(config, output)
+}
+
+func TestCalculate(t *testing.T) {
+	inputObject := Input{
+		10,
+		[]float32{1.01, 2.02},
+		[]int{1, 4},
+	}
+	output := Output{
+		30.300001,
+		4,
+		[]float32{1, 1.01, 2.02, 4},
+	}
+	outputObject := Calculate(inputObject)
+	if output.SumResult != outputObject.SumResult {
+		t.Error("SumResult are not equal")
+	}
+
+	if len(output.SortedInputs) != len(outputObject.SortedInputs) {
+		t.Error("SortedInputs are not equal")
+	}
+
+	for i := 0; i < len(output.SortedInputs); i++ {
+		if output.SortedInputs[i] != outputObject.SortedInputs[i] {
+			t.Error("SortedInputs are not equal")
+		}
+	}
+
+	if output.MulResult != outputObject.MulResult {
+		t.Error("MulResult are not equals")
+	}
+}
